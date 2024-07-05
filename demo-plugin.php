bc754c6da80737c5cafb00fa9e3fdd4a44299786
Joseph Farruggio <joey@josephallendesign.com>
2024-07-05 15:56:47 -0500

    Create demo-plugin.php

---
+++ demo-plugin.php
@@ -0,0 +1,15 @@
+/*
+ * Plugin Name:       Demo Plugin
+ * Plugin URI:        https://wppm.io
+ * Description:       Demo the WPPM private distribution
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Joey Farruggio
+ * Author URI:        https://joeyfarruggio.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
